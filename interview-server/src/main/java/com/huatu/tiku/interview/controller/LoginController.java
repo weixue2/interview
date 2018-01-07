24de15039f630550fd9373bd28fc09a8cfd53a56
zhouwei <13522569562@163.com>
2018-01-07 21:04:46 +0800

    3

--- interview-server/src/main/java/com/huatu/tiku/interview/controller/LoginController.java
+++ interview-server/src/main/java/com/huatu/tiku/interview/controller/LoginController.java
@@ -21,20 +21,32 @@ import java.io.PrintWriter;
  **/
 @RestController
 @Slf4j
-@RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@RequestMapping
 public class LoginController {
     @GetMapping(value = "")
-    public Object checkSignature(@RequestParam(name = "signature" ,required = false) String signature  ,
+    public void checkSignature(@RequestParam(name = "signature" ,required = false) String signature  ,
                                  @RequestParam(name = "nonce",required = false) String  nonce ,
                                  @RequestParam(name = "timestamp",required = false) String  timestamp ,
-                                 @RequestParam(name = "echostr",required = false) Object  echostr){
+                                 @RequestParam(name = "echostr",required = false) Object  echostr,HttpServletResponse resp){
         // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
         log.info("-------开始验证----------signature:{},nonce:{},timestamp:{},echostr:{}",signature,nonce,timestamp,echostr);
+        try {
+
+            PrintWriter out = resp.getWriter();
         if (SignUtil.checkSignature(signature, timestamp, nonce)) {
             log.info("接入成功");
-            return echostr;
+          //  return echostr;
+
+                out.print(echostr);
+                return;
+
         }
-        log.error("接入失败");
-        return echostr;
+            log.error("接入失败");
+            out.print(echostr);
+        } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+      //  return echostr;
     }
 }
