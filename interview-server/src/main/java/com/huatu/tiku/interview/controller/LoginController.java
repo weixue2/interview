c1190976cc9a2075d8a062c77b039189c0fe784f
zhouwei <13522569562@163.com>
2018-01-07 15:42:10 +0800

    3

--- interview-server/src/main/java/com/huatu/tiku/interview/controller/LoginController.java
+++ interview-server/src/main/java/com/huatu/tiku/interview/controller/LoginController.java
@@ -20,17 +20,18 @@ import java.io.PrintWriter;
 @RestController
 @Slf4j
 public class LoginController {
-    @GetMapping(value = "wx")
+    @GetMapping(value = "")
     public String checkSignature(@RequestParam(name = "signature" ,required = false) String signature  ,
                                  @RequestParam(name = "nonce",required = false) String  nonce ,
                                  @RequestParam(name = "timestamp",required = false) String  timestamp ,
                                  @RequestParam(name = "echostr",required = false) String  echostr){
         // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        log.info("-------开始验证----------");
         if (SignUtil.checkSignature(signature, timestamp, nonce)) {
             log.info("接入成功");
             return echostr;
         }
         log.error("接入失败");
-        return "";
+        return echostr;
     }
 }
