a058cde78141b78147b002ff72c8d2b7830a2dbd
zhouwei <13522569562@163.com>
2018-01-08 15:07:54 +0800

    3

--- interview-server/src/main/java/com/huatu/tiku/interview/controller/CoreController.java
+++ interview-server/src/main/java/com/huatu/tiku/interview/controller/CoreController.java
@@ -17,6 +17,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author zhouwei
@@ -37,13 +40,41 @@ public class CoreController {
 
 
    // @PostMapping(value = "", produces = "application/xml; charset=UTF-8")
-    @PostMapping(value = "")
+    @PostMapping("process")
     public Object post(HttpServletRequest req) {
         // 调用核心业务类接收消息、处理消息跟推送消息
         log.info("--------------core-------------------");
         String respMessage = coreService.processRequest(req);
         return respMessage;
     }
+
+    @GetMapping(value = "process")
+    public void checkSignature(@RequestParam(name = "signature" ,required = false) String signature  ,
+                               @RequestParam(name = "nonce",required = false) String  nonce ,
+                               @RequestParam(name = "timestamp",required = false) String  timestamp ,
+                               @RequestParam(name = "echostr",required = false) Object  echostr,HttpServletResponse resp){
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        log.info("-------开始验证----------signature:{},nonce:{},timestamp:{},echostr:{}",signature,nonce,timestamp,echostr);
+        try {
+
+            PrintWriter out = resp.getWriter();
+            if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+                log.info("接入成功");
+                //  return echostr;
+
+                out.print(echostr);
+                return;
+
+            }
+            log.error("接入失败");
+            out.print(echostr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //  return echostr;
+    }
+
 
     @GetMapping("token")
     public String getToken() {
