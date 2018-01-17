91287862313322909c5c5838a6bacc3ec2fddd8d
zhouwei <13522569562@163.com>
2018-01-17 17:22:21 +0800

    安全

--- interview-server/src/main/java/com/huatu/tiku/interview/spring/conf/web/WebSecurityConfig.java
+++ interview-server/src/main/java/com/huatu/tiku/interview/spring/conf/web/WebSecurityConfig.java
@@ -33,17 +33,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //前端开发完成，放开下面的注释
         http.csrf().disable()
                 .authorizeRequests()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-//                .antMatchers("/end/**")
-//                .authenticated()
-//                .anyRequest()
-//                .permitAll()
+                .antMatchers("/end/**")
+                .authenticated()
+                .anyRequest()
+                .permitAll()
                 .and()
-                .cors();
-//                .and()
+                .cors()
+                .and()
 //                .formLogin()
 //                .loginPage("/auth/tologin")
 //                .loginProcessingUrl("/auth/login")
@@ -54,32 +53,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .logoutUrl("/auth/logout")
 //                .logoutSuccessUrl("/auth/success?logout")
 //                .and()
-//                .rememberMe()
-//                .rememberMeCookieName(WebParamConsts.REMEMBER_ME_COOKIE)
-//                .rememberMeParameter(WebParamConsts.REMEMBER_ME_PARAM)
-//                .key(applicationName)
-//                .tokenRepository(new InMemoryTokenRepositoryImpl())
-//                .and()
-//                .exceptionHandling();
-//                .accessDeniedPage("/auth/denied")
-//                .and()
-//                .addFilterBefore(new CaptchaFilter("/auth/login"), UsernamePasswordAuthenticationFilter.class);
+               // .rememberMe()
+                //.rememberMeCookieName(WebParamConsts.REMEMBER_ME_COOKIE)
+                //.rememberMeParameter(WebParamConsts.REMEMBER_ME_PARAM)
+             //   .key(applicationName)
+             //   .tokenRepository(new InMemoryTokenRepositoryImpl())
+            //    .and()
+                .exceptionHandling();
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        //取消拦截静态资源
-        web.ignoring().antMatchers("/","/**/*.html","/**/*.css","/**/*.js","/**/*.ico","/img/**","/fonts/**","/l10n/**","/**/*.woff")
+        //取消拦截静态资源   ///wx/api/** 开发使用
+        web.ignoring().antMatchers("/wx/api/**","/","/**/*.html","/**/*.css","/**/*.js","/**/*.ico","/img/**","/fonts/**","/l10n/**","/**/*.woff")
                 .antMatchers("/validcode/img","/echo/**","/download/**");
     }
 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("admin").roles("admin")
-                .and()
-                .withUser("guest").password("guest").roles("guest");
+//        auth.inMemoryAuthentication()
+//                .withUser("admin").password("admin").roles("admin")
+//                .and()
+//                .withUser("guest").password("guest").roles("guest");
     }
 
     /**
