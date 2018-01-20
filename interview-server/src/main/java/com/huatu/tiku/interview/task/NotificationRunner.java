19bdb8258fd40b40a191856f234f4c8fb00e0401
zhenyang_294 <zai_wu_chun_qiu@163.com>
2018-01-20 16:19:36 +0800

    模板消息

--- interview-server/src/main/java/com/huatu/tiku/interview/task/NotificationRunner.java
+++ interview-server/src/main/java/com/huatu/tiku/interview/task/NotificationRunner.java
@@ -49,9 +49,10 @@ public class NotificationRunner {
     @Autowired
     WechatTemplateMsgService templateMsgService;
 
-    @Scheduled(fixedDelay = 2 * 60 * 1000)
+    @Scheduled(fixedDelay = 2 * 10 * 1000)
     public void GetNotification() {
         List<NotificationType> list = notifyService.findByPushTime();
+        System.out.println(list.size());
         if (!list.isEmpty()) {
             List<ReadingTemp> rts = new ArrayList<>();
             for (NotificationType mr : list) {
@@ -98,7 +99,7 @@ public class NotificationRunner {
                     templateMsg.setData(
                             MyTreeMap.createMap(
                                     new TemplateMap("first", WechatTemplateMsg.item("今日热点已新鲜出炉~", "#000000")),
-                                    new TemplateMap("keyword1", WechatTemplateMsg.item(u.getName(), "#000000")),
+//                                    new TemplateMap("keyword1", WechatTemplateMsg.item(u.getName(), "#000000")),
                                     new TemplateMap("keyword2", WechatTemplateMsg.item(notification.getTitle(), "#000000")),
                                     new TemplateMap("remark", WechatTemplateMsg.item("华图在线祝您顺利上岸！", "#000000"))
                             )
@@ -106,6 +107,18 @@ public class NotificationRunner {
                     break;
                 }
                 case 3: {
+                    templateMsg = new WechatTemplateMsg(u.getOpenId(), TemplateEnum.ReportHint);
+                    templateMsg.setUrl(BasicParameters.ReportHintURL+notification.getId());
+                    templateMsg.setData(
+                            MyTreeMap.createMap(
+                                    new TemplateMap("first", WechatTemplateMsg.item("亲爱的"+u.getName()+"同学，您购买的《2018国考封闭特训班》课程即将开课，请务必及时报到。", "#000000")),
+//                                    new TemplateMap("keyword1", WechatTemplateMsg.item("2018国考封闭特训班", "#000000")),
+                                    new TemplateMap("keyword2", WechatTemplateMsg.item("2018年2月2日", "#000000")),
+                                    new TemplateMap("keyword3", WechatTemplateMsg.item("北京", "#000000")),
+                                    new TemplateMap("keyword4", WechatTemplateMsg.item("400-817-6111", "#000000")),
+                                    new TemplateMap("remark", WechatTemplateMsg.item("如有疑问，请及时与我们取得联系", "#000000"))
+                            )
+                    );
                     break;
                 }
             }
