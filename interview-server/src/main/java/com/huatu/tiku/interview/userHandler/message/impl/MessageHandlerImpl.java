73277cbf1b3dd599b587fbe7c8857ed1255abf20
zhenyang_294 <zai_wu_chun_qiu@163.com>
2018-01-20 17:10:21 +0800

    模板消息1

--- interview-server/src/main/java/com/huatu/tiku/interview/userHandler/message/impl/MessageHandlerImpl.java
+++ interview-server/src/main/java/com/huatu/tiku/interview/userHandler/message/impl/MessageHandlerImpl.java
@@ -46,7 +46,7 @@ public class MessageHandlerImpl implements MessageHandler {
         String accessToken = redisTemplate.opsForValue().get(WeChatUrlConstant.ACCESS_TOKEN_KEY);
 //        accessToken = "6_xsXvmd5iX-22XaVXgCmlUzJ8V6YKssh7XfaVtZXe6GdzSydUPcKT8i4G2ULClF9Th9wmQQ9LUOGrZI8kxj330SApNk9HcEYSei1sD9F7daYj7Q3IryQQffHC9IMLIYgAGASPF";
         System.out.println("accessToken:"+accessToken);
-        if(requestMap.get("Content").equals("?")){
+//        if(requestMap.get("Content").equals("?")){
             //            String templateMsgJson = WechatTemplateMsg.getJson(TemplateEnum.No_2,requestMap);
             WechatTemplateMsg templateMsg = new WechatTemplateMsg(TemplateEnum.DailyReport,requestMap);
 
@@ -55,33 +55,33 @@ public class MessageHandlerImpl implements MessageHandler {
                     accessToken,
                     templateMsgJson);
             return null;
-        }
-        if(!requestMap.get("Content").equals("get")){
-//            //这个是直接生成String
-////            String templateMsgJson = WechatTemplateMsg.getJson(TemplateEnum.No_2,requestMap);
-//            WechatTemplateMsg templateMsg = new WechatTemplateMsg(TemplateEnum.No_2,requestMap);
-//
-//            String templateMsgJson = JsonUtil.toJson(templateMsg);
-//            TemplateMsgResult msgResult = templateMsgService.sendTemplate(
-//                    accessToken,
-//                    templateMsgJson);
-//            return null;
-            NewsMessage nm = new NewsMessage(requestMap);
-            List<Article> as = new ArrayList<>();
-            Article a = new Article();
-            a.setTitle("呵呵！!");
-            a.setDescription("点击图文可以跳转到华图首页");
-            a.setPicUrl("http://p1.music.126.net/_mEC5ZpzngngbBioF8dm4Q==/109951162973202394.jpg");
-            //这里跳转前端验证
-            a.setUrl("http://music.163.com/song?id=498040743&userid=84550482");
-            as.add(a);
-            nm.setArticleCount(as.size());
-            nm.setArticles(as);
-            return MessageUtil.MessageToXml(nm);
-        }
-        // TODO 这里还要做特定字符验证，以及正则验证
-        TextMessage tm = new TextMessage("暂无验证",requestMap);
-        return MessageUtil.MessageToXml(tm);
+//        }
+//        if(!requestMap.get("Content").equals("get")){
+////            //这个是直接生成String
+//////            String templateMsgJson = WechatTemplateMsg.getJson(TemplateEnum.No_2,requestMap);
+////            WechatTemplateMsg templateMsg = new WechatTemplateMsg(TemplateEnum.No_2,requestMap);
+////
+////            String templateMsgJson = JsonUtil.toJson(templateMsg);
+////            TemplateMsgResult msgResult = templateMsgService.sendTemplate(
+////                    accessToken,
+////                    templateMsgJson);
+////            return null;
+//            NewsMessage nm = new NewsMessage(requestMap);
+//            List<Article> as = new ArrayList<>();
+//            Article a = new Article();
+//            a.setTitle("呵呵！!");
+//            a.setDescription("点击图文可以跳转到华图首页");
+//            a.setPicUrl("http://p1.music.126.net/_mEC5ZpzngngbBioF8dm4Q==/109951162973202394.jpg");
+//            //这里跳转前端验证
+//            a.setUrl("http://music.163.com/song?id=498040743&userid=84550482");
+//            as.add(a);
+//            nm.setArticleCount(as.size());
+//            nm.setArticles(as);
+//            return MessageUtil.MessageToXml(nm);
+//        }
+//        // TODO 这里还要做特定字符验证，以及正则验证
+//        TextMessage tm = new TextMessage("暂无验证",requestMap);
+//        return MessageUtil.MessageToXml(tm);
     }
     @Override
     public String ImageMessageHandler(Map<String, String> requestMap) {
