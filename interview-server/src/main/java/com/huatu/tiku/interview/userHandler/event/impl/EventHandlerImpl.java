b01bf0746b64080968cd1163719e600fcde0398e
zhaoxi <18701244063@163.com>
2018-01-22 14:52:36 +0800

    report if else

--- interview-server/src/main/java/com/huatu/tiku/interview/userHandler/event/impl/EventHandlerImpl.java
+++ interview-server/src/main/java/com/huatu/tiku/interview/userHandler/event/impl/EventHandlerImpl.java
@@ -4,18 +4,21 @@ import com.huatu.tiku.interview.constant.BasicParameters;
 import com.huatu.tiku.interview.constant.WXStatusEnum;
 import com.huatu.tiku.interview.entity.Article;
 import com.huatu.tiku.interview.entity.message.NewsMessage;
+import com.huatu.tiku.interview.entity.po.LearningReport;
 import com.huatu.tiku.interview.entity.po.NotificationType;
 import com.huatu.tiku.interview.entity.po.SignIn;
 import com.huatu.tiku.interview.entity.po.User;
+import com.huatu.tiku.interview.repository.LearningReportRepository;
 import com.huatu.tiku.interview.repository.NotificationTypeRepository;
 import com.huatu.tiku.interview.repository.SignInRepository;
+import com.huatu.tiku.interview.repository.UserRepository;
 import com.huatu.tiku.interview.service.UserService;
 import com.huatu.tiku.interview.userHandler.event.EventHandler;
 import com.huatu.tiku.interview.util.MessageUtil;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutNewsMessage;
-import org.aspectj.weaver.patterns.NotTypePattern;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
@@ -40,7 +43,11 @@ public class EventHandlerImpl implements EventHandler {
     @Autowired
     private SignInRepository signInRepository;
     @Autowired
+    UserRepository userRepository;
+    @Autowired
     private NotificationTypeRepository notificationTypeRepository;
+    @Autowired
+    private LearningReportRepository learningReportRepository;
 
     @Override
     public String subscribeHandler(Map<String, String> requestMap) {
@@ -130,18 +137,49 @@ public class EventHandlerImpl implements EventHandler {
                     .toXml();
         }else if("dailyReport".equals(requestMap.get("EventKey"))){
             //推送学员学习报告
-            NewsMessage nm = new NewsMessage(requestMap);
-            List<Article> as = new ArrayList<>();
-            Article a = new Article();
-            a.setTitle("今日学习报告已更新。");
-            a.setDescription("请点击“详情”查看报告完整信息");
-            a.setPicUrl(BasicParameters.IMAGE_SUBSCRIBE_001);
-            //用户openID写死在链接中
-            a.setUrl(BasicParameters.DailyReportURL+requestMap.get("FromUserName"));
-            as.add(a);
-            nm.setArticleCount(as.size());
-            nm.setArticles(as);
-            return MessageUtil.MessageToXml(nm);
+            //校验用户状态 抱歉，您尚未填写个人信息，无法核实您的学员身份~
+            String openId = requestMap.get("FromUserName");
+
+            User user = userRepository.findByOpenIdAndStatus(openId, WXStatusEnum.Status.NORMAL.getStatus());
+
+
+            if(user == null ){
+                log.info("校验用户状态 抱歉，您尚未填写个人信息，无法核实您的学员身份~");
+                str = WxMpXmlOutMessage
+                        .TEXT()
+                        .content("校验用户状态 抱歉，您尚未填写个人信息，无法核实您的学员身份~")
+                        .fromUser(requestMap.get("ToUserName"))
+                        .toUser(requestMap.get("FromUserName"))
+                        .build().toXml();
+                return str;
+            }else{
+                //判断报告是否已经生成
+                List<LearningReport> learningReports = learningReportRepository.findByOpenIdOrderByIdAsc(openId);
+                if(CollectionUtils.isEmpty(learningReports)){
+                    log.info("学习报告尚未生成~");
+                    str = WxMpXmlOutMessage
+                            .TEXT()
+                            .content("学习报告尚未生成~")
+                            .fromUser(requestMap.get("ToUserName"))
+                            .toUser(requestMap.get("FromUserName"))
+                            .build().toXml();
+                    return str;
+                }
+
+                NewsMessage nm = new NewsMessage(requestMap);
+                List<Article> as = new ArrayList<>();
+                Article a = new Article();
+                a.setTitle("今日学习报告已更新。");
+                a.setDescription("请点击“详情”查看报告完整信息");
+                a.setPicUrl(BasicParameters.IMAGE_SUBSCRIBE_001);
+                //用户openID写死在链接中
+                a.setUrl(BasicParameters.DailyReportURL+requestMap.get("FromUserName"));
+                as.add(a);
+                nm.setArticleCount(as.size());
+                nm.setArticles(as);
+                return MessageUtil.MessageToXml(nm);
+            }
+
         }else if ("user_info".equals(requestMap.get("user_info"))) {
 
             WxMpXmlOutNewsMessage.Item item = new WxMpXmlOutNewsMessage.Item();
