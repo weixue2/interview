d7013271ad65ee6f98ca685da0de315cf647f167
郑毅 <125964590@qq.com>
2018-01-23 21:43:15 +0800

    login okqwe

--- interview-server/src/main/java/com/huatu/tiku/interview/config/WebSecurityConfig.java
+++ interview-server/src/main/java/com/huatu/tiku/interview/config/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig extends WebMvcConfigurerAdapter {
 
         // 排除配置
         addInterceptor.excludePathPatterns("/error");
-        addInterceptor.excludePathPatterns("/wx/end/auth/login**");
+        addInterceptor.excludePathPatterns("/end/auth/*");
 
         // 拦截配置
         addInterceptor.addPathPatterns("/end/**");
