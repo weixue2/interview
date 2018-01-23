7e538f5d1c46ded68c64ba57fd01886e49c9ffea
zhenyang_294 <zai_wu_chun_qiu@163.com>
2018-01-23 19:17:21 +0800

    url

--- interview-server/src/main/java/com/huatu/tiku/interview/task/GetReadingTableRunner.java
+++ interview-server/src/main/java/com/huatu/tiku/interview/task/GetReadingTableRunner.java
@@ -1,7 +1,18 @@
 package com.huatu.tiku.interview.task;
 
+import com.alibaba.fastjson.JSON;
+import com.huatu.tiku.interview.entity.dto.ReadingTemp;
+import com.huatu.tiku.interview.entity.po.NotificationType;
+import com.huatu.tiku.interview.repository.NotificationTypeRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author ZhenYang
@@ -12,30 +23,30 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class GetReadingTableRunner{
 
-//    @Autowired
-//    NotificationTypeRepository readingRepository;
-//
-//    @Autowired
-//    StringRedisTemplate stringRedisTemplate;
-//
-//    //获取晨读。。
-//    @Scheduled(fixedDelay = 2 * 3600 * 1000)
-//    public void getReading() throws InterruptedException {
-//        List<NotificationType> all = readingRepository.findAll();
-//        for (NotificationType nt : all){
-//
-//        }
-//
-//        if(!all.isEmpty()){
-//            List<ReadingTemp> rts = new ArrayList<>();
-//            for (NotificationType mr:all){
-//                rts.add(new ReadingTemp(mr.getId(),mr.getPushTime(),true,mr.getType()));
-//            }
-//            String json = JSON.toJSONString(rts);
-//            System.out.println(rts);
-////            Thread.sleep(1000*31);
-//            stringRedisTemplate.opsForValue().set("readings",json);
-//            stringRedisTemplate.expire("readings",2 * 3600 * 1000, TimeUnit.SECONDS);
-//        }
-//    }
+    @Autowired
+    NotificationTypeRepository readingRepository;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    //获取晨读。。
+    @Scheduled(fixedDelay = 2 * 10 * 1000)
+    public void getReading() throws InterruptedException {
+        List<NotificationType> all = readingRepository.findAll();
+        for (NotificationType nt : all){
+
+        }
+
+        if(!all.isEmpty()){
+            List<ReadingTemp> rts = new ArrayList<>();
+            for (NotificationType mr:all){
+                rts.add(new ReadingTemp(mr.getId(),mr.getPushTime(),true,mr.getType()));
+            }
+            String json = JSON.toJSONString(rts);
+            System.out.println(rts);
+            Thread.sleep(1000*31);
+            stringRedisTemplate.opsForValue().set("readings",json);
+            stringRedisTemplate.expire("readings",2 * 3600 * 1000, TimeUnit.SECONDS);
+        }
+    }
 }
