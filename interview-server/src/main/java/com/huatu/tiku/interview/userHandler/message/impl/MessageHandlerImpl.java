b3be90e67eb006de197420fe0d8ca8b221547260
zhenyang_294 <zai_wu_chun_qiu@163.com>
2018-01-24 17:33:44 +0800

    删除无用的

--- interview-server/src/main/java/com/huatu/tiku/interview/userHandler/message/impl/MessageHandlerImpl.java
+++ interview-server/src/main/java/com/huatu/tiku/interview/userHandler/message/impl/MessageHandlerImpl.java
@@ -65,7 +65,7 @@ public class MessageHandlerImpl implements MessageHandler {
                     templateMsgJson);
             return null;
         }
-        if(requestMap.get("Content").equals("2")){
+//        if(requestMap.get("Content").equals("2")){
 //            //这个是直接生成String
 ////            String templateMsgJson = WechatTemplateMsg.getJson(TemplateEnum.No_2,requestMap);
 //            WechatTemplateMsg templateMsg = new WechatTemplateMsg(TemplateEnum.No_2,requestMap);
@@ -75,34 +75,34 @@ public class MessageHandlerImpl implements MessageHandler {
 //                    accessToken,
 //                    templateMsgJson);
 //            return null;
-            NewsMessage nm = new NewsMessage(requestMap);
-            List<Article> as = new ArrayList<>();
-            Article a = new Article();
-            a.setTitle("呵呵！!");
-            a.setDescription("点击图文可以跳转到华图首页");
-            a.setPicUrl("http://p1.music.126.net/_mEC5ZpzngngbBioF8dm4Q==/109951162973202394.jpg");
-            //这里跳转前端验证
-            log.info("手机验证路径："+phoneCheck+requestMap.get("FromUserName"));
-            a.setUrl(phoneCheck + "openId="+requestMap.get("FromUserName"));
-            as.add(a);
-            nm.setArticleCount(as.size());
-            nm.setArticles(as);
-            return MessageUtil.MessageToXml(nm);
-        }
-        if(requestMap.get("Content").equals("3")){
-            WechatTemplateMsg templateMsg = new WechatTemplateMsg(requestMap.get("FromUserName"), TemplateEnum.MorningReading);
-                    templateMsg.setUrl(notifyView+6);
-                    templateMsg.setData(
-                            MyTreeMap.createMap(
-                                    new TemplateMap("first", WechatTemplateMsg.item("今日热点已新鲜出炉~", "#000000")),
-                                    new TemplateMap("keyword1", WechatTemplateMsg.item("不知道", "#000000")),
-                                    new TemplateMap("keyword2", WechatTemplateMsg.item("不知道", "#000000")),
-                                    new TemplateMap("remark", WechatTemplateMsg.item("华图在线祝您顺利上岸！", "#000000"))
-                            )
-                    );
-            templateMsgService.sendTemplate(accessToken, JsonUtil.toJson(templateMsg));
-            return null;
-        }
+//            NewsMessage nm = new NewsMessage(requestMap);
+//            List<Article> as = new ArrayList<>();
+//            Article a = new Article();
+//            a.setTitle("呵呵！!");
+//            a.setDescription("点击图文可以跳转到华图首页");
+//            a.setPicUrl("http://p1.music.126.net/_mEC5ZpzngngbBioF8dm4Q==/109951162973202394.jpg");
+//            //这里跳转前端验证
+//            log.info("手机验证路径："+phoneCheck+requestMap.get("FromUserName"));
+//            a.setUrl(phoneCheck + "openId="+requestMap.get("FromUserName"));
+//            as.add(a);
+//            nm.setArticleCount(as.size());
+//            nm.setArticles(as);
+//            return MessageUtil.MessageToXml(nm);
+//        }
+//        if(requestMap.get("Content").equals("3")){
+//            WechatTemplateMsg templateMsg = new WechatTemplateMsg(requestMap.get("FromUserName"), TemplateEnum.MorningReading);
+//                    templateMsg.setUrl(notifyView+6);
+//                    templateMsg.setData(
+//                            MyTreeMap.createMap(
+//                                    new TemplateMap("first", WechatTemplateMsg.item("今日热点已新鲜出炉~", "#000000")),
+//                                    new TemplateMap("keyword1", WechatTemplateMsg.item("不知道", "#000000")),
+//                                    new TemplateMap("keyword2", WechatTemplateMsg.item("不知道", "#000000")),
+//                                    new TemplateMap("remark", WechatTemplateMsg.item("华图在线祝您顺利上岸！", "#000000"))
+//                            )
+//                    );
+//            templateMsgService.sendTemplate(accessToken, JsonUtil.toJson(templateMsg));
+//            return null;
+//        }
         if(!requestMap.get("Content").equals("2")){
             System.out.println("手机验证路径："+phoneCheck+requestMap.get("FromUserName"));
             NewsMessage nm = new NewsMessage(requestMap);
